95da6ccfc8f35cae0da97e86e1b65b03d98e3504
randonhyman316 <randonhyman316@gmail.com>
2019-04-10 21:30:58 -0500

    k

--- Classwork/GameManager/GameManager.Sql/SqlGameDatabase.cs
+++ Classwork/GameManager/GameManager.Sql/SqlGameDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -27,24 +28,24 @@ namespace GameManager.Sql
                 cmd.CommandText = "AddGame";
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
-                //Add parameter 1
+                //Add parameter 1 - long way when you need control over parameter
                 var parameter = new SqlParameter("@name", System.Data.SqlDbType.NVarChar);
                 parameter.Value = game.Name;
                 cmd.Parameters.Add(parameter);
 
-                //Add parameter 2
+                //Add parameter 2 - quick way when you just need type/value
                 cmd.Parameters.AddWithValue("@description", game.Description);
                 cmd.Parameters.AddWithValue("@price", game.Price);
                 cmd.Parameters.AddWithValue("@completed", game.Completed);
                 cmd.Parameters.AddWithValue("@owned", game.Owned);
 
+                // (int)cmd.ExecuteScalar();
+                // cmd.ExecuteScalar() as int;  //reference types 
                 var result = Convert.ToInt32(cmd.ExecuteScalar());
 
                 game.Id = result;
                 return game;
             };
-
-            throw new NotImplementedException();
         }
 
         private SqlConnection GetConnection()
@@ -54,12 +55,57 @@ namespace GameManager.Sql
 
         protected override void DeleteCore( int id )
         {
-            throw new NotImplementedException();
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+
+                //var cmd = new SqlCommand("", connection);
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = "DeleteGame";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@id", id);
+
+                //No results
+                cmd.ExecuteNonQuery();
+            };
         }
 
         protected override IEnumerable<Game> GetAllCore()
         {
-            //throw new NotImplementedException();
+            var ds = new DataSet();
+
+            using (var conn = GetConnection())
+            {
+                var cmd = new SqlCommand("GetGames", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                
+                var da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                
+                da.Fill(ds);
+
+                //If you wanted to update
+                //da.Update(ds);
+            };
+
+            //Disconnected from DB
+            //ds.Tables[0].Rows[0][0] = "Old";
+            //ds.Tables[0].Rows[0]["Name"] = "Old";
+            var table = ds.Tables.OfType<DataTable>().FirstOrDefault();
+            if (table != null)
+            {
+                return from r in table.Rows.OfType<DataRow>()
+                       select new Game() {
+                           Id = Convert.ToInt32(r[0]),  //Ordinal, convert
+                           Name = r["Name"].ToString(), //By name, convert
+                           Description = r.IsNull("description") ? "" : r["description"].ToString(), //handle DB nulls
+                           Price = r.Field<decimal>("Price"),
+                           Owned = r.Field<bool>("Owned"),
+                           Completed = r.Field<bool>("Completed"),
+                       };
+            };
+
             return Enumerable.Empty<Game>();
         }
 
@@ -69,9 +115,34 @@ namespace GameManager.Sql
             return null;
         }
 
-        protected override Game UpdateCore( int id, Game newGame )
+        protected override Game UpdateCore( int id, Game game )
         {
-            throw new NotImplementedException();
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+
+                //var cmd = new SqlCommand("", connection);
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = "UpdateGame";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                //Add parameter 1 - long way when you need control over parameter
+                var parameter = new SqlParameter("@name", System.Data.SqlDbType.NVarChar);
+                parameter.Value = game.Name;
+                cmd.Parameters.Add(parameter);
+
+                //Add parameter 2 - quick way when you just need type/value
+                cmd.Parameters.AddWithValue("@description", game.Description);
+                cmd.Parameters.AddWithValue("@price", game.Price);
+                cmd.Parameters.AddWithValue("@completed", game.Completed);
+                cmd.Parameters.AddWithValue("@owned", game.Owned);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                //No results
+                cmd.ExecuteNonQuery();
+            };
+
+            return game;
         }
     }
 }
